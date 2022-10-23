2b583bde6ffe35d33479ebbc58f1c4865e667782
jenterr <116439389+jenterr@users.noreply.github.com>
2022-10-23 21:42:39 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '乖宝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'o4kc26Cw_-pDxX_X8va7q94z-42w',
+      id: 'o4kc26Cw_-pDxX_X8va7q94z-42w','o4kc26FyG7H-XJ3hOB3U3xX0q1gQ',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '	p8E2DRRLnZwXHPaK_fkf1BkYyFJCvaup25BP9JR9dOU',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
@@ -29,7 +29,7 @@ const USER_CONFIG = {
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1999', date: '06-29',
+          type: '*生日', name: '乖宝', year: '1999', date: '06-29',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
