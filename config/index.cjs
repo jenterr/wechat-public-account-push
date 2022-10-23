b568831785731c40ce014fe836f36e14de79aca0
jenterr <116439389+jenterr@users.noreply.github.com>
2022-10-23 21:43:47 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '乖宝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'o4kc26Cw_-pDxX_X8va7q94z-42w','o4kc26FyG7H-XJ3hOB3U3xX0q1gQ',
+      id: 'o4kc26Cw_-pDxX_X8va7q94z-42w',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '	p8E2DRRLnZwXHPaK_fkf1BkYyFJCvaup25BP9JR9dOU',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
